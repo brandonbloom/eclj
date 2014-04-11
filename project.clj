9a948f6b1e65f9e3d0b68943f88e1366ca56002d
Brandon Bloom <brandon@brandonbloom.name>
2014-04-11 15:28:24 -0400

    Update project.clj metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject eclj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Extensible Clojure"
+  :url "https://github.com/brandonbloom/eclj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
