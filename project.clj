283d0b4471ed0ee3067c573abb1df1a5dd9c0636
Brandon Bloom <brandon@brandonbloom.name>
2014-04-10 22:54:22 -0400

    Use custom nrepl for eclj support

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles
+  {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.4-SNAPSHOT"]]}})
