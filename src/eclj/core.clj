8e5298136f555534068ec2dc0779a0107754c26d
Brandon Bloom <brandon@brandonbloom.name>
2014-04-10 22:54:40 -0400

    Some work on ns loading

--- src/eclj/core.clj
+++ src/eclj/core.clj
@@ -2,11 +2,11 @@
   (:refer-clojure :only [])
   (:require [eclj.eval]
             [eclj.env :as env]
-            [eclj.ns :refer (copy-vars)]
+            [eclj.ns :as ns]
             [eclj.interpret.cps :refer (interpreter)]))
 
-(copy-vars 'clojure.core :exclude '#{
-  eval case deftype defrecord defprotocol
+(ns/copy-vars 'clojure.core :exclude '#{
+  eval case deftype defrecord defprotocol refer-clojure
 })
 
 ;;XXX This ties the recursive knot for eclj.fn/fn-apply
@@ -15,7 +15,8 @@
 (defn eval [x]
   (eclj.eval/eval x (env/ns-env)))
 
-(defmacro case [e & clauses]
+(defmacro case
+  [e & clauses]
   (let [default? (odd? (count clauses))
         cases (partition 2 (if default? (butlast clauses) clauses))
         table (into {} (map vec cases))]
@@ -31,3 +32,37 @@
 (defmacro deftype [& args] `(clojure.core/eval '~&form))
 (defmacro defrecord [& args] `(clojure.core/eval '~&form))
 (defmacro defprotocol [& args] `(clojure.core/eval '~&form))
+
+(defmacro refer-clojure
+  "Same as (refer 'clojure.eclj <filters>)"
+  [& filters]
+  `(eclj.core/refer '~'eclj.core ~@filters))
+
+(defmacro ns
+  "Sets *ns* to the namespace named by name (unevaluated), creating it
+  if needed.  references can be zero or more of: (:refer-clojure ...)
+  (:require ...) (:use ...) (:import ...) (:load ...) (:gen-class)
+  with the syntax of refer-clojure/require/use/import/load/gen-class
+  respectively, except the arguments are unevaluated and need not be
+  quoted. (:gen-class ...), when supplied, defaults to :name
+  corresponding to the ns name, :main true, :impl-ns same as ns, and
+  :init-impl-ns true. All options of gen-class are
+  supported. The :gen-class directive is ignored when not
+  compiling. If :gen-class is not supplied, when compiled only an
+  nsname__init.class will be generated. If :refer-clojure is not used, a
+  default (refer 'clojure.core) is used.  Use of ns is preferred to
+  individual calls to in-ns/require/use/import:
+
+  (ns foo.bar
+    (:refer-clojure :exclude [ancestors printf])
+    (:require (clojure.contrib sql combinatorics))
+    (:use (my.lib this that))
+    (:import (java.util Date Timer Random)
+             (java.sql Connection Statement)))"
+  {:arglists '([name docstring? attr-map? references*])
+   :added "1.0"}
+  [name & references]
+
+  (prn (ns/parse &form))
+  (inc 5)
+  nil)
--- src/eclj/ns.clj
+++ src/eclj/ns.clj
@@ -1,6 +1,19 @@
 (ns eclj.ns)
 
+;;XXX This is likely all kinds of broken. Consider dynamic vars!
 (defn copy-vars [ns & {:keys [exclude]}]
   (doseq [[sym var] (ns-publics ns)
           :when (not (exclude sym))]
     (intern *ns* (with-meta sym (meta var)) @var)))
+
+(defn parse [[_ sym & body :as form]]
+  (let [doc (when (string? (first body)) (first body))
+        refs (if doc (next body) body)
+        sym (if doc (vary-meta sym assoc :doc doc) sym)
+        metadata (when (map? (first refs)) (first refs))
+        refs (if metadata (next refs) refs)
+        sym (if metadata (vary-meta sym merge metadata) sym)]
+    {:name sym
+     :meta (meta sym)
+     :refs (vec (remove #(= :gen-class (first %)) refs))
+     :gen-class (first (filter #(= :gen-class (first %)) refs))}))
