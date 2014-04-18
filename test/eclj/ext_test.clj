51a70f4a7a169e259c8a54cb782b2f5a67fda4bc
Brandon Bloom <brandon@brandonbloom.name>
2014-04-18 19:38:02 -0400

    comment out some tests for a bit

--- test/eclj/ext_test.clj
+++ test/eclj/ext_test.clj
@@ -4,19 +4,39 @@
 
 ;;TODO: Assertions
 
-(eclj.core/require 'eclj.ext :reload)
+(comment
 
-(eclj.ext/eval 1)
-(eclj.ext/eval '(+ 2 4))
+  (eclj.core/require 'eclj.ext :reload)
 
-(eclj.ext/eval '(eclj.ext/handle-with
-                  (fn [effect]
-                    (when (= (:op effect) :answer)
-                      {:op :answer :value (inc (:value effect))}))
-                  5))
+  (eclj.ext/eval 1)
+  (eclj.ext/eval '(+ 2 4))
 
-(eclj.ext/eval '(eclj.ext/handle-with
-                  (fn [effect]
-                    (when (= (:op effect) :foo)
-                      {:op :answer :value :bar}))
-                  (eclj.ext/raise {:op :foo})))
+  (eclj.ext/eval '(eclj.ext/handle-with
+                    (fn [effect]
+                      (when (= (:op effect) :answer)
+                        {:op :answer :value (inc (:value effect))}))
+                    5))
+
+  (eclj.ext/eval '(eclj.ext/handle-with
+                    (fn [effect]
+                      (when (= (:op effect) :foo)
+                        {:op :answer :value :bar}))
+                    (eclj.ext/raise {:op :foo})))
+
+  (eclj.core/eval
+    '(eclj.ext/handle-with
+       (fn [{:keys [op k] :as effect}]
+         (println "outer")
+         (fipp.edn/pprint effect)
+         (println)
+         (when (= op :foo)
+           {:op :answer :value (k 123)}))
+       (eclj.ext/handle-with
+         (fn [{:keys [op k] :as effect}]
+           (println "inner")
+           (fipp.edn/pprint effect)
+           (println)
+           nil)
+         (eclj.ext/raise {:op :foo}))))
+
+)
