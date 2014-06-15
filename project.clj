332c3b40a897fae0251a458558573224485bcb3c
Brandon Bloom <brandon@brandonbloom.name>
2014-06-15 13:41:27 -0400

    remove dev dependencies

--- project.clj
+++ project.clj
@@ -4,7 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.reader "0.8.4"]]
-  :profiles
-  {:dev {:dependencies [[org.clojure/tools.nrepl "0.2.4-SNAPSHOT"]
-                        [fipp "0.4.1"]]}})
+                 [org.clojure/tools.reader "0.8.4"]])
